52302a689a66dd056dc0723d6b6351e8de8f5492
lchsk <mjlechowski@gmail.com>
2014-03-15 21:10:57 +0100

    # First working version # Bigger dictionary

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import random
+import sys
+import getopt
 
 class Data(object):
     
@@ -10,6 +12,13 @@ class Data(object):
         self.my_words = {}
         
         self.words_no = 0
+        
+        # 1 - simple mode : translation from foreign language to native
+        # 2 - simple mode : translation from native language to foreign
+        self._playing_mode = -1
+        
+        # current dictionary for chosen playing mode
+        self._current_set = {}
     
     def load_db(self):
         try:
@@ -23,7 +32,7 @@ class Data(object):
                 translation = line.split('=')
                 foreign_word = translation[0]
                 t = translation[1].split(';')
-                self.foreign[foreign_word] = t
+                self.foreign[foreign_word] = [x.replace('\n', '') for x in t ]
         
         print str(self.words_no) + ' entries found.'
         
@@ -37,15 +46,51 @@ class Data(object):
                 self.my_words[key] = []
                 self.my_words[key].append(k)
                 
+    def start_game(self):
+        try:                                
+            opts, args = getopt.getopt(sys.argv[1:], "h12", ['help', 'foreign', 'native']) 
+            
+            #print opts, sys.argv
+            
+            for opt, arg in opts:                
+                if opt in ("-h", "--help"):      
+                    print 'help'                  
+                elif opt in ('-1', '--foreign'):
+                    self._playing_mode = 1
+                    self._current_set = self.foreign
+                elif opt in ('-2', '--native'):
+                    self._playing_mode = 2
+                    self._current_set = self.my_words
+                else:
+                    print 'unhandled option'               
+            
+        except getopt.GetoptError:                                    
+            print 'heeeeelp'
+                
     def play(self):
         
         print 'Type QUIT to exit the program'
         
-        #while(True):
+        answer = ''
+        
+        while(answer != 'QUIT'):
             
-            #ans = raw_input(question + ': ')
-            #print ans
+            question = random.choice(self._current_set.keys())
+            correct = self._current_set[question]
+            
+            
+            answer = raw_input(question + ': ')
+            if answer in correct:
+                print '\tCORRECT!'
+            else:
+                answers = ''
+                for ind, ans in enumerate(correct):
+                    answers += ans
+                    if ind < len(correct) - 1:
+                        answers += ', '
+                print '\tWRONG!\tCorrect: ' + answers
                 
+        print 'Learn more later'
         
 
 def main():
@@ -53,6 +98,7 @@ def main():
     
     data = Data()
     data.load_db()
+    data.start_game()
     data.play()
     
     #for k, v in data.my_words.items():
